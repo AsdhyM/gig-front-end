7fbd19b5d12114c894f6be4d54fd4cdd5a69f4b9
AsdhyM <asdhyme@gmail.com>
2024-12-21 21:25:17 +1000

    feat: Profile icon and dropdown menu on Navbar

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,6 +4,7 @@ import carol_image from './carol_image.png'
 import david_image from './david_image.png'
 import steve_image from './steve_image.png'
 import tom_image from './tom_image.png'
+import profile_icon from './profile_icon.png'
 
 
 export const assets = {
@@ -12,5 +13,6 @@ export const assets = {
     carol_image,
     david_image,
     steve_image,
-    tom_image
+    tom_image,
+    profile_icon
 }
